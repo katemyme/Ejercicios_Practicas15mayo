0c403b04c062b73f9c493292691274a1f9d6ed56
ValcocerLopez <vmalcocer@uamv.edu.ni>
2026-05-15 16:43:01 -0600

    Feat: Agregando el caso uno del menu ( Agregar estudiante )

--- src/main/java/ni/edu/uam/ArrayList/Segundo_ejercicio.java
+++ src/main/java/ni/edu/uam/ArrayList/Segundo_ejercicio.java
@@ -1,5 +1,6 @@
 package ni.edu.uam.ArrayList;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Segundo_ejercicio {
@@ -14,5 +15,25 @@ public class Segundo_ejercicio {
                 "3. Ver Lista Actualizada\n" +
                 "4. Salir\n" +
                 "Seleccione una opción:";
+
+        while (continuar) {
+            String opcionStr = JOptionPane.showInputDialog(menu);
+            if (opcionStr == null) {
+                break;
+            }
+            try {
+                int opcion = Integer.parseInt(opcionStr);
+                switch (opcion) {
+                    case 1:
+                        String nuevoNombre = JOptionPane.showInputDialog("Ingrese el nombre del estudiante:");
+                        estudiantes.add(nuevoNombre);
+                        JOptionPane.showMessageDialog(null, "¡Estudiante agregado con éxito!");
+
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Por favor, seleccione una opción numérica válida.");
+            }
+        }
     }
 }
