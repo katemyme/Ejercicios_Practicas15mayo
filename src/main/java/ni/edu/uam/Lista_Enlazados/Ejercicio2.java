15f4e6c7a16f744192ad322956761168dc03c9fb
Andre <andre.guido@uamv.edu.ni>
2026-05-15 16:52:10 -0600

    feat: Creación del primer ejercicio.

---
+++ src/main/java/ni/edu/uam/Lista_Enlazados/Ejercicio2.java
@@ -0,0 +1,16 @@
+package ni.edu.uam.Lista_Enlazados;
+
+// Crear un LinkedList de nombres y agregar un nombre al inicio y otro al final.
+
+import java.util.LinkedList;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        LinkedList<String> nombres = new LinkedList<>();
+        int opcion;
+        do{
+            opcion =
+
+        }while (opcion != 3);
+    }
+}
