7bdd26872db3d7a70d945e15d66f2040df8dd638
ValcocerLopez <vmalcocer@uamv.edu.ni>
2026-05-15 16:31:44 -0600

    Agregando el menu del segundo ejercicio

---
+++ src/main/java/ni/edu/uam/ArrayList/Segundo_ejercicio.java
@@ -0,0 +1,18 @@
+package ni.edu.uam.ArrayList;
+
+import java.util.ArrayList;
+
+public class Segundo_ejercicio {
+    public static void main(String[] args) {
+        // Creamos el ArrayList para almacenar cadenas de texto (nombres)
+        ArrayList<String> estudiantes = new ArrayList<>();
+        boolean continuar = true;
+
+        String menu = "MENÚ DE ESTUDIANTES\n" +
+                "1. Agregar Estudiante\n" +
+                "2. Eliminar Estudiante\n" +
+                "3. Ver Lista Actualizada\n" +
+                "4. Salir\n" +
+                "Seleccione una opción:";
+    }
+}
